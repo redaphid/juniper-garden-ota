cce3b400019beb90691d27f4dd2fc336e8d05f07
redaphid <iam@aaronherres.com>
2022-12-05 14:21:38 -1000

    ota starting

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
-function someHandleCredsFunction ({ ssid, password }) {
-  trace(`someHandleCredsFunction: ssid: ${ssid}, password: ${password} \n`);
-  if(password === 'orangejuice') return true
-  return false
-}
+import OTA from "ota";
 
-import ImprovWifi from "improv-wifi-mod";
-let server = new ImprovWifi({
-  deviceName: "M",
-  onCredentialsRecieved: someHandleCredsFunction
-});
+// Request definition: /home/redaphid/Projects/moddable/modules/network/http/http.js
+import {Request} from "http";
+
+const host = "http://192.168.1.103:8080";
+const path = "/YOUR/PATH/HERE.bin"
+
+const request = new Request({host, path});
+trace(`requesting ${host}${path}\n`);
+request.callback = function(message, value, etc) {
+  trace(`message: ${message}, value: ${value}, etc: ${etc}\n`);
+	switch (message) {
+		case Request.status:
+			if (200 !== value)
+				throw new Error("unexpected http status");
+			break;
+
+		case Request.header:
+			if ("content-length" === value) {
+				try {
+					this.byteLength = parseInt(etc);
+					this.ota = new OTA({byteLength: this.byteLength});
+					this.received = 0;
+				}
+				catch (e) {
+					throw new Error("unable to start OTA: " + e);
+				}
+			}
+			break;
+
+		case Request.responseFragment: {
+			const bytes = this.read(ArrayBuffer);
+			this.received += bytes.byteLength;
+			this.ota.write(bytes);
+			trace(`received ${this.received} of ${this.byteLength}\n`);
+			} break;
+
+		case Request.responseComplete:
+			this.ota.complete();
+			trace("ota complete\n");
+			break;
+
+		default:
+			if (message < 0) {
+				this.ota.cancel();
+				throw new Error("http error");
+			}
+			break;
+	}
+}
